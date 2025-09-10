9f95f644cd330d2047babbf00f0e5efb88e117b6
Rennan Mendes Serenza <rennan.serenza@sanesul.ms.gov.br>
2025-09-10 16:40:56 -0400

    [UPDATE] - Implementação de um enumerador para especificação de tipo.

--- Dima/Dima.Core/Models/Transaction.cs
+++ Dima/Dima.Core/Models/Transaction.cs
@@ -1,4 +1,6 @@
-﻿namespace Dima.Core.Models;
+﻿using Dima.Core.Enums;
+
+namespace Dima.Core.Models;
 
 public class Transaction
 {
@@ -8,7 +10,7 @@ public class Transaction
     public DateTime CreatedAt { get; set; } = DateTime.Now;
     public DateTime? PaidOrReceivedAt { get; set; }
 
-    public int Type { get; set; }
+    public ETransactionType Type { get; set; } = ETransactionType.Withdraw;
 
     public decimal Amount { get; set; }
 
