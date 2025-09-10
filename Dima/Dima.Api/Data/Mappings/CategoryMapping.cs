fc21c6aa4d5d3871cad5cfecfe90f4248a97c5d1
Rennan Mendes Serenza <rennan.serenza@sanesul.ms.gov.br>
2025-09-10 17:39:54 -0400

    [New] - Implementação inicial do mapeamento de modelos.

---
+++ Dima/Dima.Api/Data/Mappings/CategoryMapping.cs
@@ -0,0 +1,31 @@
+﻿using Dima.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Dima.Api.Data.Mappings
+{
+    public class CategoryMapping : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("Categories");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Title)
+                .IsRequired()
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(80);
+
+            builder.Property(x => x.Description)
+                .IsRequired()
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(255);
+
+            builder.Property(x => x.UserId)
+                .IsRequired()
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(160);
+        }
+    }
+}
