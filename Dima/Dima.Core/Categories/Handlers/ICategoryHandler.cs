db08f2bcdb8862d1301c65bda1a4a10663ccf81f
Rennan Mendes Serenza <rennan.serenza@sanesul.ms.gov.br>
2025-09-12 17:21:29 -0400

    [NEW] - Criação da interface do handler base de Categoria.

---
+++ Dima/Dima.Core/Categories/Handlers/ICategoryHandler.cs
@@ -0,0 +1,14 @@
+﻿using Dima.Core.Categories.Requests;
+using Dima.Core.Models;
+using Dima.Core.Responses;
+
+namespace Dima.Core.Categories.Handlers;
+
+public interface ICategoryHandler
+{
+    Task<Response<Category?>> CreateAsync(CreateCategoryRequest request);
+    Task<Response<Category?>> UpdateAsync(UpdateCategoryRequest request);
+    Task<Response<Category?>> DeleteAsync(DeleteCategoryRequest request);
+    Task<Response<Category?>> GetByIdAsync(GetCategoryByIdRequest request);
+    Task<Response<List<Category>>> GetAllAsync(GetAllCategoriesRequest request);
+}
